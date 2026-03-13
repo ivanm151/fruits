e0668c6853338298552528a7f782d39182b4e44b
ivanm151 <ivmedv5@gmail.com>
2026-03-14 00:28:15 +0700

    server docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Для правильной работы в Docker
+  server: {
+    host: true, // слушаем все интерфейсы
+    port: 5173,
+  },
+  preview: {
+    host: true,
+    port: 80,
+  }
 })
